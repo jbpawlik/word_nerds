9d3bf6c3ec0ac8be6c55d001f9942688cea8420f
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-23 08:41:22 -0700

    create new Word instance; spec passing

--- lib/word.rb
+++ lib/word.rb
@@ -1,15 +1,26 @@
 class Word
+  attr_reader :id
+  attr_accessor :name
 
     @@words = {}
     @@total_rows = 0
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
-    @id = attributes.fetc(:id) || @@total_rows += 1
+    @id = attributes.fetch(:id) || @@total_rows += 1
   end
 
   def self.all
     @@words.values
   end
-  
+
+  def ==(word_to_compare)
+    self.name == word_to_compare.name
+  end
+
+
+  def save
+    @@words[self.id] = Word.new({:name => self.name, :id => self.id})
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,12 +3,33 @@ SimpleCov.start
 require 'rspec'
 require 'word'
 
-describe('#Word') do
+describe('#word') do
   
   describe('.all') do
     it('returns an empty array when there are no words') do
     expect(Word.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('saves a word') do
+      word = Word.new({:name => 'reff', :id => nil})
+      word.save
+      expect(Word.all).to(eq([word]))
+    end
+  end
+
+
+  # describe('#save') do
+  #   it('saves a word') do
+  #     word = Word.new({:name => 'reff', :id => nil})
+  #     word.save
+  #     expect(Word.all).to(eq([word]))
+  #     word2 = Word.new({:name => 'alis', :id => nil})
+  #     word2.save
+  #     expect(Word.all).to(eq([word]))
+  #   end
+  # end
+
 
 end
